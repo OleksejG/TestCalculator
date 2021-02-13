e238289c20de70e0a5c11a41f028f70d5055e798
OleksejG <78223921+OleksejG@users.noreply.github.com>
2021-02-13 14:36:19 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -8,11 +8,18 @@ let pack2=6;
 function quantity(){let square=document.getElementById('lenght__calc').value*document.getElementById('width__calc').value;
 let calculate=square / block ;
 let num__pack=calculate / pack;
-let num__ost=calculate % pack;
-let num__pack2=num__ost / pack2;
 let num__pack_mat=Math.trunc(num__pack);
+let num__ost=calculate % pack;
+let num__pack2__ost=num__ost / pack2;
+let num__pack2=calculate / pack2;
 
- console.log(('You will need '),(num__pack_mat),('packs of 8 pcs'),('and'),(num__pack2),('packs of 6 pcs') ) ;
+if(calculate===6,calculate===12){alert('You will need  '+num__pack2+' packs of 6 pcs') ;}
+
+else if(num__pack>1){alert('You will need  '+num__pack_mat+' packs of 8 pcs  '+'and '+num__pack2__ost+' packs of 6 pcs') ;
+} else{alert('You will need  '+num__pack2+' packs of 6 pcs') ;
+}
+
+ 
     
 
 }
