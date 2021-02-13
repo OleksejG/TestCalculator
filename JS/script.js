8d855e748a05a3c10e27f4a9204c5fca8f0429f4
OleksejG <78223921+OleksejG@users.noreply.github.com>
2021-02-13 13:00:49 +0300

    calculator

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function() 
-	{$('.heder__burger').click(function(event) {
-		$('.heder__burger,.heder__menu').toggleClass('active');
-	});
-});
+
+let block=4;
+
+let pack=8;
+let pack2=6;
+
+
+function quantity(){let square=document.getElementById('lenght__calc').value*document.getElementById('width__calc').value;
+let calculate=square / block ;
+let num__pack=calculate / pack;
+let num__ost=calculate % pack;
+let num__pack2=num__ost / pack2;
+let num__pack_mat=Math.trunc(num__pack);
+
+ console.log(('You will need '),(num__pack_mat),('packs of 8 pcs'),('and'),(num__pack2),('packs of 6 pcs') ) ;
+    
+
+}
